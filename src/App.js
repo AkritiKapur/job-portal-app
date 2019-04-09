099f14c096cc3d12ee7c79035a7e084a6f73df82
AkritiKapur <akritikapur1@gmail.com>
2019-04-09 12:27:17 -0600

    React component and redux store added to sampleapp

--- src/App.js
+++ src/App.js
@@ -1,28 +1,44 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import logo from './logo.svg';
+import { sampleAction } from './actions/sampleAction';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+ render() {
+  return (
+   <div className="App">
+    <header className="App-header">
+     <img src={logo} className="App-logo" alt="logo" />
+     <h1 className="App-title">Welcome to React</h1>
+    </header>
+    <pre>
+        {
+          JSON.stringify(this.props)
+        }
+    </pre>
+    <button onClick={this.sampleAction}>Test redux action</button>
+    <p className="App-intro">
+     To get started, edit <code>src/App.js</code> and save to reload
+    </p>
+   </div>
+  );
+ }
+
+ sampleAction = (event) => {
+  this.props.sampleAction();
+ }
+
 }
 
-export default App;
+const mapStateToProps = state => ({
+  ...state
+ })
+
+ const mapDispatchToProps = dispatch => ({
+  sampleAction: () => dispatch(sampleAction())
+ })
+
+
+
+ export default connect(mapStateToProps, mapDispatchToProps)(App);
