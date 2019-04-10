974a0b23970407a70886a50c0fa1a375766e0d04
AkritiKapur <akritikapur1@gmail.com>
2019-04-09 21:01:37 -0600

    bootstrap dependencies, card changes added

--- src/candidateDashboard/Dashboard.js
+++ src/candidateDashboard/Dashboard.js
@@ -1,11 +1,33 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class Dashboard extends Component {
     render() {
         return (
             <div className="candidate-dashboard">
-            
+                <div class="row">
+                    <div class="col-sm-6">
+                        <div class="card">
+                            <div class="card-body">
+                                <h5 class="card-title">Job 1: SDE</h5>
+                                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                                <a href="#" class="btn btn-primary">Apply</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-6">
+                        <div class="card">
+                            <div class="card-body">
+                                <h5 class="card-title">Job 2: Professor</h5>
+                                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                                <a href="#" class="btn btn-primary">Apply</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
 }
+
+export default connect()(Dashboard);
--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import $ from 'jquery';
+import Popper from 'popper.js';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux'
 import configureStore from './store';
 import './index.css';
 import App from './App';
+import Dashboard from './candidateDashboard/Dashboard';
 import * as serviceWorker from './serviceWorker';
+
 ReactDOM.render(
  <Provider store={configureStore()}>
-  <App />
+  <Dashboard />
  </Provider>,
  document.getElementById('root')
 );
