4c6a08787b1e21b1a4396f612438ea50e02c223e
AkritiKapur <akritikapur1@gmail.com>
2019-04-11 19:27:41 -0600

    Cleanup and Tabs added for applications and  jobs.

--- src/App.js
+++ src/App.js
@@ -1,44 +1,60 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import Tabs from 'react-bootstrap/Tabs'
+import Tab from 'react-bootstrap/Tab'
 import logo from './logo.svg';
 import { sampleAction } from './actions/sampleAction';
 import './App.css';
+import Dashboard from './candidateDashboard/Dashboard';
+import Header from './header/header';
+import Search from './search/search';
 
 class App extends Component {
- render() {
-  return (
-   <div className="App">
-    <header className="App-header">
-     <img src={logo} className="App-logo" alt="logo" />
-     <h1 className="App-title">Welcome to React</h1>
-    </header>
-    <pre>
-        {
-          JSON.stringify(this.props)
-        }
-    </pre>
-    <button onClick={this.sampleAction}>Test redux action</button>
-    <p className="App-intro">
-     To get started, edit <code>src/App.js</code> and save to reload
-    </p>
-   </div>
-  );
- }
+  constructor(props, context) {
+    super(props, context);
+    this.state = {
+      key: 'dashboard',
+    };
+  }
 
- sampleAction = (event) => {
-  this.props.sampleAction();
- }
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        
+        <div className="container-fluid">
+          <Search />
+          <Tabs
+            id="controlled-tab-home"
+            activeKey={this.state.key}
+            onSelect={key => this.setState({ key })}
+          >
+            <Tab eventKey="dashboard" title="Available Jobs">
+              <Dashboard />
+            </Tab>
+            <Tab eventKey="applied" title="Applications">
+              <Dashboard />
+            </Tab>
+          </Tabs>
+        </div>
+      </div>
+    );
+  }
+
+//  sampleAction = (event) => {
+//   this.props.sampleAction();
+//  }
 
 }
 
-const mapStateToProps = state => ({
-  ...state
- })
+// const mapStateToProps = state => ({
+//   ...state
+//  })
 
- const mapDispatchToProps = dispatch => ({
-  sampleAction: () => dispatch(sampleAction())
- })
+//  const mapDispatchToProps = dispatch => ({
+//   sampleAction: () => dispatch(sampleAction())
+//  })
 
 
-
- export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default connect()(App);
+//  export default connect(mapStateToProps, mapDispatchToProps)(App);
