d37294baf4d8cf7919654e65f39a51b6e4b09c1c
AkritiKapur <akritikapur1@gmail.com>
2019-04-17 20:34:30 -0600

    skeleton signup component

---
+++ src/signup/Signup.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class Signup extends Component {
+    render() {
+
+    }
+}
+
+export default connect()(Signup);
