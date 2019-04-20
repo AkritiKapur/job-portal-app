bfd4027206f99953c98e893d7aaf18392d35fd3f
AkritiKapur <akritikapur1@gmail.com>
2019-04-19 21:27:08 -0600

    redux debug tool changes

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,5 +1,8 @@
 import { combineReducers } from 'redux';
 import sampleReducer from './sampleReducer';
+import authenticationReducer from '../authentication/reducer';
+
 export default combineReducers({
- sampleReducer
+ sampleReducer,
+ authenticationReducer
 });
